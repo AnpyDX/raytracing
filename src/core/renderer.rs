a80d0bcfd59faf244e97c9f607a55fe582f4e577
anpyd <anpyd@outlook.com>
2024-12-19 20:15:14 +0800

    remove DebugLevel. Now debug info will be always printed.

--- src/core/renderer.rs
+++ src/core/renderer.rs
@@ -4,6 +4,7 @@ use super::backends::{
     STDrivenRenderer, STDrivenRendererConfig 
 };
 
+#[derive(Debug)]
 pub enum BackendConfig {
     /// Renderer running on single thread.
     CPUDrivenS,
@@ -11,23 +12,10 @@ pub enum BackendConfig {
     CPUDrivenM(u32),
 }
 
-pub enum DebugLevel {
-    /// Running renderer with fully debug info.
-    /// - Renderer configure.
-    /// - Rendering progress.
-    Full, 
-    /// Running renderer with printing progress number only.
-    Porting,
-    /// Running renderer without any debug info.
-    None
-}
-
 /// Renderer configurations
 pub struct RendererConfig {
     /// Renderer backend.
     pub backend: BackendConfig,
-    /// Debug level.
-    pub dbg_level: DebugLevel,
     /// The maximum number of ray bounce depth.
     pub max_depth: u32,
     /// The number of sample-times per pixel.
@@ -45,32 +33,15 @@ impl Renderer {
     }
 
     pub fn render(&mut self, scene: &Scene, camera: &Camera, screen: (u32, u32)) -> Vec<Vec3> {
-        let backend_info = self.init_backend(screen);
-
-        if let DebugLevel::Full = self.config.dbg_level {
-            println!("Debug informations:");
-            println!("> backend: {}", backend_info);
-            println!("> entity number: {}", scene.entities.len());
-            println!("> resolution: {}x{}", screen.0, screen.1);
-            println!("> quality: {} spp, {} depth", self.config.spp, self.config.max_depth);
-        }
+        self.init_backend(screen);
 
         let mut index = 0;
         let fb_size = (screen.0 * screen.1) as f64;
         let renderer = self.backend.as_mut().unwrap();
 
         for rays in camera.rays(screen, self.config.spp) {
-            match self.config.dbg_level {
-                DebugLevel::Full => {
-                    let progress = index as f64 / fb_size;
-                    Self::show_progress(progress);
-                },
-                DebugLevel::Porting => {
-                    let progress = index as f64 / fb_size;
-                    println!("{}", progress);
-                },
-                DebugLevel::None => {}
-            }
+            let progress = index as f64 / fb_size;
+            Self::show_progress(progress);
 
             let task = RenderTask { rays, scene, index };
             renderer.submit(task);
@@ -81,9 +52,7 @@ impl Renderer {
         return renderer.fetch();
     }
 
-    fn init_backend(&mut self, screen: (u32, u32)) -> String {
-        let backend_info;
-
+    fn init_backend(&mut self, screen: (u32, u32)) {
         match self.config.backend {
             BackendConfig::CPUDrivenS => {
                 let renderer = STDrivenRenderer::new(
@@ -94,13 +63,10 @@ impl Renderer {
                 );
 
                 self.backend = Some(Box::new(renderer));
-                backend_info = "CPU driven, single-thread".to_string();
             },
 
             BackendConfig::CPUDrivenM(_) => { panic!("muti-thread renderer hasn't been impl yet!") }
         }
-
-        return backend_info;
     }
 
     fn show_progress(percentage: f64) {
